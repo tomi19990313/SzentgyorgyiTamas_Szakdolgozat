e9620a6846099957b894627c48fcf7df0e34f299
Tamás Szentgyörgyi <szentgyorgyitamas99@gmail.com>
2021-03-04 17:56:43 +0100

    Create the WPF application with Tobii.Interaction package

---
+++ SkillTest/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using Tobii.Interaction;
+using Tobii.Interaction.Wpf;
+
+namespace SkillTest
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ SkillTest/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace SkillTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
