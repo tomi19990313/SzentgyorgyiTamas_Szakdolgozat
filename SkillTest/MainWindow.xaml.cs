d433487911e4623708361ad6daa0c7a31940097d
Tamás Szentgyörgyi <szentgyorgyitamas99@gmail.com>
2021-04-01 00:02:09 +0200

    Create Result class, and StartButton

--- SkillTest/MainWindow.xaml.cs
+++ SkillTest/MainWindow.xaml.cs
@@ -1,12 +1,21 @@
 ﻿using System.Windows;
+using System.Windows.Threading;
 
 namespace SkillTest
 {
     public partial class MainWindow : Window
     {
+        private DispatcherTimer Timer = new DispatcherTimer();  // Timer for the time periods
+        private Result Result;  // Result class object for representing the correct, and the received results of the test
+        public int GazeNumber { get; set; }  // Gaze number of the current test
+
+
         public MainWindow()
         {
             InitializeComponent();
+
+            //this.Result = new Result(GazeNumber);
+            this.Result = new Result(10);  // Temporary value 10
         }
     }
 }
---
+++ SkillTest/Result.cs
@@ -0,0 +1,44 @@
+﻿// Result class for representing the correct, and the received results of the test
+
+
+namespace SkillTest
+{
+    public class Result
+    {
+        private string[] CorrectResults;   // The expected results
+        private string[] ReceivedResults;  // The received results
+        public int GazeNumber { get; }     // The gaze number of the current test, and the length of the result arrays
+
+
+        // In the constructor set the number of array elements, and fill up the arrays with null values
+        public Result(int gazeNumber)
+        {
+            this.CorrectResults = new string[gazeNumber];
+            this.ReceivedResults = new string[gazeNumber];
+            this.GazeNumber = gazeNumber;
+        }
+
+
+        // Getter, and setter for CorrectResults
+        public string getCorrectResults(int index)
+        {
+            return this.CorrectResults[index];
+        }
+
+        public void setCorrectResults(int index, string value)
+        {
+            this.CorrectResults[index] = value;
+        }
+
+        // Getter, and setter for ReceivedResults
+        public string getReceivedResults(int index)
+        {
+            return this.ReceivedResults[index];
+        }
+
+        public void setReceivedResults(int index, string value)
+        {
+            this.ReceivedResults[index] = value;
+        }
+    }
+}
